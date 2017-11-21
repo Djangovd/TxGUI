ef66504ddced45bba9f9fb162137f03489991bbb
Djangovd <Djangovd@users.noreply.github.com>
2017-11-21 18:01:35 +0000

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -30,66 +30,59 @@ Created on Wed Oct  4 14:41:09 2017
 from Tkinter import *#Tk, Entry, Button, Label, INSERT, Canvas
 root = Tk()
 
-
 Fields = ['Version', '# inputs', 'Previous TXout hash', 'Previous TXout idx', 'scriptSig', 'Sequence', '# outputs', 'value', 'scriptPubKey', 'locktime']
+#Sizes(bytes): 4,       1-9,           32,                    4,                 - ,           4 ,          1 - 9,    8 , 4
+FieldSize = [8, 18, 64, 8, 128, 8, 18, 16, 128, 8]
 Ents = []
 Labs = []
 Frams = []
 
 for field in Fields:
-    Frams.append(Frame(root))
-    Ents.append(Entry(Frams[Fields.index(field)], relief=SUNKEN, width=120))#.grid(row=Fields.index(field),column=1))
-    Labs.append(Label(Frams[Fields.index(field)], text=field, relief=RIDGE, width=25))#.grid(row=Fields.index(field),column=0))
-
-for item in Frams:
-    item.pack(fill=X, expand=True)
-
+#    Frams.append(Frame(root))
+ #.grid(row=Fields.index(field),column=1))
+    Labs.append(Label(root, text=field, relief=RIDGE, width=25).grid(row=Fields.index(field),column=0))
+    Ents.append(Entry(root, relief=SUNKEN, width=FieldSize[Fields.index(field)]))
+#    Ents.append(Entry(Frams[Fields.index(field)], relief=SUNKEN, width=FieldSize[Fields.index(field)]))#.grid(row=Fields.index(field),column=1))
+#    Labs.append(Label(Frams[Fields.index(field)], text=field, relief=RIDGE, width=25))#.grid(row=Fields.index(field),column=0))
+#
+#for item in Frams:
+#    item.pack(fill=X, expand=True)
+#
 for item in Ents:
-    item.grid(row=Ents.index(item),column=1)
-        
-for item in Labs:
-    item.grid(row=Labs.index(item),column=0)
-    
-for item in Ents:
-    item.pack(side=RIGHT, expand=True)
-    
-for item in Labs:
-    item.pack(side=LEFT, expand=True)
+    item.grid(column=1, row=Ents.index(item), sticky=W)
+#        
+#for item in Labs:
+#    item.grid(column=0, row=Labs.index(item), sticky=W)
+#    
+#for item in Ents:
+#    item.pack(side=RIGHT, expand=True)
+#    
+#for item in Labs:
+#    item.pack(side=LEFT, expand=True)
+#Label(root, text="First Name").grid(row=0)
+#Label(root, text="Last Name").grid(row=1)
 
-    ###
-e1 = Entry(root)
-e2 = Entry(root)
+#e1 = Entry(root)
+#e2 = Entry(root)
 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-# Create single line text entry box
-e1.pack()
-
-# Specifying character position in entry
-# - END: After last character of entry widget
-# - ANCHOR: The beginning of the current selection
-# - INSERT: Current text cursor position
-# - "@x": Mouse coordinates
-
-# Insert some default text
-#entry.insert(INSERT, 'Hello, world!')
-e1.insert(INSERT, 'Testing!')
-
-l1 = Label(root, text="First")#.grid(row=3, column=0)
-l1.grid(row=0, column=4)
-l1.pack()
 
 # Print the contents of entry widget to console
 def print_content():
     #print(entry.get())
-    print(e1.get())
+    #print(e1.get())
+    strng = ''
+    for item in Ents:
+        strng += item.get()
+    print strng
+
 
 # Create a button that will print the contents of the entry
 button = Button(root, text='Print content', command=print_content)
-button.pack()
-
+#button = Button(root, text='Print content', command=print_content)
+#button.pack()
+button.grid(column=0, row=len(Ents)+2, sticky=W)
 #entry2 = Entry(root)
-e2.pack()
-e2.insert(INSERT,'yo, sup?!')
+#e2.pack()
+#e2.insert(10,'yo, sup?!')
 
 root.mainloop()
